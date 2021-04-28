cadf119f4f761a77bdd049f611bde0691876fd4b
Your Name <you@example.com>
2021-04-28 18:23:36 +0200

    aktualita

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+   
+    
   })
   win.loadFile('index.html')
 }
@@ -21,5 +19,5 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
-  }
+   }
 })
